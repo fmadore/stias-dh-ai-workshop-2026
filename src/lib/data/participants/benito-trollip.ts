9c92a81a6796f5a031bbc85aa655071e613a5746
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-07-07 11:02:40 +0200

    content(participants): add photo for Benito Trollip

--- src/lib/data/participants/benito-trollip.ts
+++ src/lib/data/participants/benito-trollip.ts
@@ -9,6 +9,7 @@ const participant: Participant = {
 	},
 	country: 'South Africa',
 	coordinates: { lat: -26.6819, lng: 27.0946 },
+	image: '/images/participants/Benito-Trollip.webp',
 	presentationId: 'fair-indigenous-languages',
 	bio: 'Benito Trollip is a Digital Humanities researcher, specialising in Afrikaans, at the South African Centre for Digital Language Resources (SADiLaR). He completed his PhD in 2022 in which he described morphological evaluative constructions in Afrikaans based on usage data in a written corpus of Afrikaans. Besides his background in Afrikaans linguistics, he is also passionate about open science and data management. His role at SADiLaR also involves presenting data management workshops and managing resource submissions to the SADiLaR repository.'
 };
