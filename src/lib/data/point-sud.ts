f524e17b8a941e91acad9c2439afc26557f4744a
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-08-14 10:30:26 +0200

    content(participants): add bio and photo for Issa Fofana

--- src/lib/data/point-sud.ts
+++ src/lib/data/point-sud.ts
@@ -39,6 +39,11 @@ export const pointSud: PointSudRepresentative[] = [
 			en: 'Point Sud, Bamako',
 			fr: 'Point Sud, Bamako'
 		},
-		country: 'ML'
+		country: 'ML',
+		image: '/images/point-sud/Issa-Fofana.webp',
+		bio: {
+			en: 'Issa Fofana is a lecturer and researcher at the University of Social Sciences and Management of Bamako (USSGB) and Co-Director of Point Sud, a research centre on local knowledge. He holds a PhD in Geography, specialising in urban spaces and societies, as well as an advanced degree in social work. His research focuses primarily on urbanisation dynamics, urban mobility, security, and the integration of agricultural markets through digital technologies. He is the author of several scientific publications, with particular interests in transformations in urban mobility, urban insecurity, and local strategies for building sustainable peace.',
+			fr: 'Issa Fofana est enseignant-chercheur à l’Université des sciences sociales et de gestion de Bamako (USSGB) et codirecteur de Point Sud, centre de recherche sur les savoirs locaux. Titulaire d’un doctorat en géographie, spécialisé en espaces et sociétés urbaines, il est également diplômé d’études supérieures en travail social. Ses recherches portent principalement sur les dynamiques d’urbanisation, la mobilité urbaine, la sécurité et l’intégration des marchés agricoles par les technologies numériques. Il est l’auteur de plusieurs publications scientifiques, avec un intérêt particulier pour les transformations de la mobilité urbaine, l’insécurité urbaine et les stratégies locales de construction d’une paix durable.'
+		}
 	}
 ];
