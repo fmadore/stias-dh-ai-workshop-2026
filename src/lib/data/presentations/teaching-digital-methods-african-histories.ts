57e2cd87007d458672798a7a94177547234038ee
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-09-04 15:56:31 +0200

    content(papers): retitle Vincent Hiribarren's session and add its abstract

--- src/lib/data/presentations/teaching-digital-methods-african-histories.ts
+++ src/lib/data/presentations/teaching-digital-methods-african-histories.ts
@@ -4,7 +4,8 @@ const presentation: Presentation = {
 	id: 'teaching-digital-methods-african-histories',
 	language: 'en',
 	authors: ['hiribarren'],
-	title: 'Teaching Digital Methods for Historians of Africa'
+	title: 'Teaching Digital History as a Historian of Africa',
+	abstract: `For the last session before our concluding synthesis, the speaker will first focus on the pedagogical challenges he faced when creating a Digital History module at King's College London. Participants will subsequently be divided into groups and will consider the best ways to teach Digital Humanities in an African context.`
 };
 
 export default presentation;
